faa903380c04077bd61278c35c32fe0b5caa5f96
uyenntt92 <touyen810@gmail.com>
2024-04-25 21:38:20 +0700

    BT2

--- src/bt2_phamvitruycap/person/Person.java
+++ src/bt2_phamvitruycap/person/Person.java
@@ -5,34 +5,36 @@ public class Person {
     public String gender;
     public String address;
     public String phone;
-    public Person(String name,int age, String gender, String address,String phone){
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
-      this.address = address;
-      this.phone = phone;
+    public Person(String name, int age, String gender, String address, String phone) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.address = address;
+        this.phone = phone;
     }
-//    public Person(String phươngAnh, int age, String Gender) {
-//    }
     public static void main(String[] args) {
         System.out.println();
     }
-    public String getName(){return name;};
-    public int getAge(){ return age;}
-    public  String getGender(){return gender;}
-    public  String getAddress(){return address;}
-    public  String getPhone(){return phone;}
-    public void getinformation () {
+    public String getName() {
+        return name;};
+    public int getAge() {
+        return age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void getinformation() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Gender: " + gender);
         System.out.println("Address: " + address);
         System.out.println("Phone: " + phone);
     }
-    public void getinfodefault (String name, int age, String gender){
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Gender: " + gender);
-    };
-      }
+}
 
