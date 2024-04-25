a71831e58e28c8df299436fdcd3b4f5365fe6073
uyenntt92 <touyen810@gmail.com>
2024-04-25 21:16:32 +0700

    BT3

---
+++ src/bt2_phamvitruycap/person/Person.java
@@ -0,0 +1,38 @@
+package bt2_phamvitruycap.person;
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+    private String address;
+    private String phone;
+    public Person(String name,int age, String gender, String address,String phone){
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+      this.address = address;
+      this.phone = phone;
+    }
+    public Person(String phươngAnh, int age, String nữ) {
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
+    public String getName(){return name;};
+    public int getAge(){ return age;}
+    public  String getGender(){return gender;}
+    public  String getAddress(){return address;}
+    public  String getPhone(){return phone;}
+    public void getinformation () {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Address: " + address);
+        System.out.println("Phone: " + phone);
+    }
+    public void getinfodefault (String name, int age, String gender){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+    };
+      }
+
---
+++ src/bt2_phamvitruycap/person/company/Company.java
@@ -0,0 +1,10 @@
+package bt2_phamvitruycap.person.company;
+
+import bt2_phamvitruycap.person.Person;
+public class Company {
+    public static void main(String[] args) {
+Person person = new Person("Hà Anh",25,"Nữ"){
+    Person.getinformation();
+};
+    };
+       }
